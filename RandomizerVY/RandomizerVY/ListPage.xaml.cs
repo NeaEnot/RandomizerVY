386f78e9135622aa708dfa2af239ef7ec5c62bda
NeaEnot <oleg.kozloss@yandex.ru>
2021-09-08 19:47:12 +0400

    Переход на следующий элемент Удаление элемента Окончательное удаление элемента Перевод фокуса при удалении элемента Удаление элемента только если ввести удаление в пустую строку

--- RandomizerVY/RandomizerVY/ListPage.xaml.cs
+++ RandomizerVY/RandomizerVY/ListPage.xaml.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -22,16 +21,84 @@ namespace RandomizerVY
 
             foreach (string str in list)
             {
-                flexLayout
-                    .Children
-                    .Add(new Entry
-                    {
-                        Text = str,
-                        HorizontalOptions = LayoutOptions.Center,
-                        WidthRequest = 300,
-                        HorizontalTextAlignment = TextAlignment.Center
-                    });
+                flexLayout.Children.Add(CreateEntry(str));
             }
+        }
+
+        private void Entry_Completed(object sender, EventArgs e)
+        {
+            Stack<Entry> stack = new Stack<Entry>();
+
+            while (flexLayout.Children.Last() != sender)
+            {
+                stack.Push((Entry)flexLayout.Children.Last());
+                flexLayout.Children.Remove(flexLayout.Children.Last());
+            }
+
+            Entry entry = CreateEntry("");
+            flexLayout.Children.Add(entry);
+            entry.Focus();
+
+            while (stack.Count > 0)
+            {
+                flexLayout.Children.Add(stack.Pop());
+            }
+        }
+
+        private void Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (e.NewTextValue == "")
+            {
+                Entry entry = sender as Entry;
+                entry.Unfocus();
+                entry.IsVisible = false;
+
+                Stack<Entry> stack = new Stack<Entry>();
+
+                while (flexLayout.Children.Last() != sender)
+                {
+                    stack.Push((Entry)flexLayout.Children.Last());
+                    flexLayout.Children.Remove(flexLayout.Children.Last());
+                }
+
+                if (flexLayout.Children.Count > 1)
+                {
+                    Entry prev = flexLayout.Children[flexLayout.Children.Count - 2] as Entry;
+
+                    prev.Focus();
+                    prev.CursorPosition = prev.Text.Length;
+                }
+
+                while (stack.Count > 0)
+                {
+                    flexLayout.Children.Add(stack.Pop());
+                }
+
+                Task.Run(() =>
+                {
+                    Thread.Sleep(500);
+                    flexLayout.Children.Remove(entry);
+                });
+            }
+        }
+
+        private Entry CreateEntry(string str)
+        {
+            Entry entry = new Entry
+            {
+                Text = "\n" + str,
+                HorizontalOptions = LayoutOptions.Center,
+                WidthRequest = 300,
+                HorizontalTextAlignment = TextAlignment.Center,
+            };
+            entry.Completed += Entry_Completed;
+            entry.TextChanged += Entry_TextChanged;
+            return entry;
+        }
+
+        private string GetEntryText(Entry entry)
+        {
+            return entry.Text.Replace("\n", "");
         }
 
         private void Button_Clicked(object sender, EventArgs e)
